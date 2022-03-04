ef111780f1c129eae1f4d9acd067c9e8bc4d4823
cheng0908 <cscheng5282@smail.nchu.edu.tw>
2022-03-04 18:51:54 +0800

    add floor detail in JetNanoDB & create gallery

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from website.models import Gallery
 
 # Register your models here.
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('filename', 'created_at')
+
+
+admin.site.register(Gallery, GalleryAdmin)
